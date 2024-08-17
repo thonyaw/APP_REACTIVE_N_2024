2c6f5698de93a5e467cd874049293cdca09d7a80
Vink64 <161389498+Vink64@users.noreply.github.com>
2024-08-17 01:04:55 +0000

    uso de estado

--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,58 @@
-import { Text, View , StyleSheet} from "react-native";
+import { useState } from "react";
+import { Text, View , StyleSheet, Button} from "react-native";
+
+const produtos = [
+  {id: 1, nome: "Schweppers", preco:5.5},
+  {id: 2, nome: "Pepsi", preco:5.5},
+  {id: 3, nome: "Guaraná Cruzeiro", preco:5.5},
+  {id: 4, nome: "Guaraná Jesus", preco:5.5},
+];
 
 export default function Index() {
+
+  let [counter,setCounter]= useState(0);
+
   return (
+
     <View
       style={estilo.container}
     >
-      <Text style={estilo.text}>Hello World!!</Text>
+      
+    {produtos.map((p)=>(
+      <View key={p.id}>
+      <Text style={estilo.title}>{p.nome}</Text>
+      <Text style={estilo.text}>{p.preco}</Text>
+      </View>
+      )
+      )}
+      <Button title={counter.toString()} onPress={()=>{clickButton()}}></Button>
     </View>
   );
+
+  function clickButton(){
+    setCounter(counter+1)
+  }
+
 }
 
 const estilo= StyleSheet.create({
   container: {
     flex: 1,
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: "flex-start",
         backgroundColor: "#0c0a0e",
+        paddingStart: 40,
   },
   text:{
     color:"#fc0377",
-    fontSize: 90,
+    fontSize: 24,
+    fontFamily: "chiller",
+  },
+  title:{
+    color: "#17ff70",
+    fontSize: 30,
+    textAlign: "left",
+    fontWeight: "bold",
     fontFamily: "chiller",
   }
 });
